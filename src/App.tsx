b7890041576bd5d883b398aacf9db0561ac58547
RoboticsDevelopmentName <RoboticsDevelopment@protonmail.com>
2023-08-10 20:56:34 -0400

    React BeastMode

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-
-import ListGroup from './components/ListGroup'
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup /></div>
+  let animals = ["Cat", "Dog", "Dragon", "Lizard"];
+  let hobbies = [
+    "Flying Drones",
+    "Collecting Bullion",
+    "Dreaming about Wifey",
+    "Playing with Animals",
+    "Eating Pizza",
+  ];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item + " selected");
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={animals}
+        heading="Choose your Favorite Animal"
+        onSelectItem={handleSelectItem}
+      />
+      <ListGroup
+        items={hobbies}
+        heading="Choose your favorite Hobby"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,41 @@
-function ListGroup() {
-  const hobbies = [
-    "Flying Drones ",
-    "Collecting Bullion",
-    "Thinking about Girlfriend",
-    "Playing with Animals",
-    "Eating Pizza",
-  ];
+//import { MouseEvent } from "react";
+import { useState } from "react";
+
+//PROPS: Input passed to a component!  Similar to function args!  Immutable
+//STATE: Data managed by a component!  Similar to local variables!  Mutable
+
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // const handleClick = (event: MouseEvent) => console.log(event); //Event Handler
+
   return (
     <>
-      <h1>Patricks Hobbies</h1>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found!</p>}
       <ul className="list-group">
-        {hobbies.map((hobby) => (
-          <li key={hobby}>{hobby}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            //Each child in a list should have a unique "Key" prop!
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
